31dc86c5c34b8d3617317bfdb487018424b7900f
Lorenzo La Rocca <60919219+LorenzoLaRocca@users.noreply.github.com>
2025-06-05 17:34:23 +0200

    Uptadet Main

--- Main.java
+++ Main.java
@@ -2,49 +2,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-         * Biblioteca biblioteca = new Biblioteca();
-         * 
-         * Libro libro = new Libro("Il Signore degli Anelli", 1937, "001");
-         * Ebook ebook = new Ebook("1984", 1949, "002");
-         * Rivista rivista = new Rivista("National Geographic", 2025, "003");
-         * 
-         * biblioteca.aggiungiRisorsa(libro);
-         * biblioteca.aggiungiRisorsa(ebook);
-         * biblioteca.aggiungiRisorsa(rivista);
-         * 
-         * Utente utente1 = new Utente("Mario", "U001");
-         * Utente utente2 = new Utente("Luigi", "U002");
-         */
-
+     
         Scanner sc = new Scanner(System.in);
-        Biblioteca biblioteca = new Biblioteca();
-        Risorsa risorsa=new Risorsa(null, 0, null);
+        Biblioteca biblioteca = new Biblioteca("Musei Vaticani");
+
         while (true) {
             System.out.println(
                     "1 per inserire una risorsa \n2 per inserire un utente \n3 stampare inventario biblioteca \n4 cercare per titolo \n5 per uscire ");
             int scelta = Integer.parseInt(sc.nextLine());
             switch (scelta) {
                 case 1:
-                    System.out
-                            .println("Che tipo di risorsa vuoi inserire? \n1 per Libro \n2 per Ebook \n3 per Rivista");
+                    System.out.println("Che tipo di risorsa vuoi inserire? \n1 per Libro \n2 per Ebook \n3 per Rivista");
                     int risorsa = Integer.parseInt(sc.nextLine());
-                    if (risorsa ==1) {
+                    if (risorsa == 1) {
                         String titolo = sc.nextLine();
                         int anno = Integer.parseInt(sc.nextLine());
                         String codice = sc.nextLine();
-                        biblioteca.aggiungiRisorsa(Risorsa libro=new Libro(titolo,anno,codice));
+                        biblioteca.aggiungiRisorsa(new Libro(titolo, anno, codice));
+                    } else if (risorsa == 2){
+                        String titolo = sc.nextLine();
+                        int anno = Integer.parseInt(sc.nextLine());
+                        String codice = sc.nextLine();
+                        biblioteca.aggiungiRisorsa(new Ebook(titolo, anno, codice));
+                    } else if (risorsa == 3) {
+                        String titolo = sc.nextLine();
+                        int anno = Integer.parseInt(sc.nextLine());
+                        String codice = sc.nextLine();
+                        biblioteca.aggiungiRisorsa(new Rivista(titolo, anno, codice));                          
                     }
                     break;
-
                 case 2:
+                    System.out.println("Inserisci il nome: ");
+                    String utente = sc.nextLine();
+                    System.out.println("Inserisci l'id: ");
+                    String id = sc.nextLine();
+                    biblioteca.aggiungiUtente(new Utente(utente, id));
                     break;
                 case 3:
+                    biblioteca.visualizzaDettagli();
                     break;
                 case 4:
+                    System.out.println("Inserisci Titolo: ");
+                    String titoloLibro = sc.nextLine();
+                    biblioteca.cercaRisorsaPerTitolo(titoloLibro);
                     break;
                 case 5:
-                    break;
+                    System.out.println("Arrivederci");
+                    sc.close();
+                    return;
             }
 
         }
