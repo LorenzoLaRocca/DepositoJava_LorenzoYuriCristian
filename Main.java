fbfd094fc5b5ab7d3a2aef2d7671370d30dbfa3b
FenixBlood <cristian_platania@hotmail.it>
2025-06-05 17:17:31 +0200

    creato switch e menu

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -15,5 +17,36 @@ public class Main {
          * Utente utente2 = new Utente("Luigi", "U002");
          */
 
+        Scanner sc = new Scanner(System.in);
+        Biblioteca biblioteca = new Biblioteca();
+        Risorsa risorsa=new Risorsa(null, 0, null);
+        while (true) {
+            System.out.println(
+                    "1 per inserire una risorsa \n2 per inserire un utente \n3 stampare inventario biblioteca \n4 cercare per titolo \n5 per uscire ");
+            int scelta = Integer.parseInt(sc.nextLine());
+            switch (scelta) {
+                case 1:
+                    System.out
+                            .println("Che tipo di risorsa vuoi inserire? \n1 per Libro \n2 per Ebook \n3 per Rivista");
+                    int risorsa = Integer.parseInt(sc.nextLine());
+                    if (risorsa ==1) {
+                        String titolo = sc.nextLine();
+                        int anno = Integer.parseInt(sc.nextLine());
+                        String codice = sc.nextLine();
+                        biblioteca.aggiungiRisorsa(Risorsa libro=new Libro(titolo,anno,codice));
+                    }
+                    break;
+
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        }
     }
 }
