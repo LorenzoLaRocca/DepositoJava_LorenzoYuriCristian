f8e7b5dd95a7d373630979aab92fb63f69adb0cd
FenixBlood <cristian_platania@hotmail.it>
2025-06-05 16:53:35 +0200

    creato main e esempio di creazione

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Biblioteca biblioteca = new Biblioteca();
+         * 
+         * Libro libro = new Libro("Il Signore degli Anelli", 1937, "001");
+         * Ebook ebook = new Ebook("1984", 1949, "002");
+         * Rivista rivista = new Rivista("National Geographic", 2025, "003");
+         * 
+         * biblioteca.aggiungiRisorsa(libro);
+         * biblioteca.aggiungiRisorsa(ebook);
+         * biblioteca.aggiungiRisorsa(rivista);
+         * 
+         * Utente utente1 = new Utente("Mario", "U001");
+         * Utente utente2 = new Utente("Luigi", "U002");
+         */
+
+    }
+}
